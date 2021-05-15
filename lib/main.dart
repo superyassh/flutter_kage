5a4a67e6be7ceffd89e63f1e9dbfad549f2948f4
superyassh <yanshukishore@gmail.com>
2021-05-15 17:02:50 +0530

    Welcome to Arcadia

--- lib/main.dart
+++ lib/main.dart
@@ -18,33 +18,3 @@ class Kage extends StatelessWidget {
     );
   }
 }
-
-class Myapp extends StatefulWidget {
-  @override
-  _State createState() => new _State();
-}
-
-class _State extends State<Myapp> {
-  int count = 0;
-
-  void incrementCounter() {
-    setState(() {
-      count++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Flutter Tutorial - googleflutter.com'),
-      ),
-      body: new Center(
-        child: new RaisedButton(
-          onPressed: () => {incrementCounter()},
-          child: new Text('Button Clicks - ${count}'),
-        ),
-      ),
-    );
-  }
-}
